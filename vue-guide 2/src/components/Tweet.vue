201ede08a9fef1600ef7027ebd2f5d6ab1446af1
devmatsuko <devmatsuko@gmail.com>
2022-05-05 16:37:55 +0900

    Make components

---
+++ vue-guide 2/src/components/Tweet.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TweetPostForm from './TweetPostForm.vue';
+import TweetList from './TweetList.vue';
+const tweets = ref([{ id: 0, description: 'Hello, world!' }, { id: 1, description: 'this is the second tweet' }])
+
+const postTweet = (description: string) => {
+  const tweet = { id: Math.random(), description }
+  tweets.value.push(tweet)
+  description = ''
+}
+
+const deleteTweet = (id: number) => {
+  tweets.value = tweets.value.filter(t => t.id !== id)
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <TweetPostForm @post-tweet="postTweet"></TweetPostForm>
+    <div class="tweet-container">
+      <p v-if="tweets.length <= 0">No tweets have been added</p>
+      <ul v-else>
+        <TweetList :tweets="tweets" @delete-tweet="deleteTweet"></TweetList>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input {
+  margin-bottom: 16px;
+}
+
+label {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
