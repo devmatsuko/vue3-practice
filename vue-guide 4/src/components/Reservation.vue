c79cefb0da4a020209acc1b7e9171c769e088098
devmatsuko <devmatsuko@gmail.com>
2022-05-06 13:35:58 +0900

    Use slot.

---
+++ vue-guide 4/src/components/Reservation.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import Card from './Card.vue';
+import Badge from './Badge.vue';
+</script>
+
+<template>
+  <div class="container">
+    <Card class="menu-card">
+      <span>Badges</span>
+      <div class="badges">
+        <Badge class="vip-badge">
+          <span>VIP</span>
+        </Badge>
+        <Badge class="normal-badge">
+          <span>NORMAL</span>
+        </Badge>
+      </div>
+    </Card>
+
+    <Card class="reservation-card">
+      <span>John Wick</span>
+      <Badge class="vip-badge">
+        <span>VIP</span>
+      </Badge>
+    </Card>
+    <Card class="reservation-card">
+      <span>Shuya Inada</span>
+      <Badge class="normal-badge">
+        <span>normal</span>
+      </Badge>
+    </Card>
+    <Card class="reservation-card">
+      <span>Silvers Leily</span>
+      <Badge class="vip-badge">
+        <span>VIP</span>
+      </Badge>
+    </Card>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.vip-badge {
+  background-color: #810036;
+  color: white;
+}
+
+.normal-badge {
+  background-color: #002c8a;
+  color: white;
+}
+
+.menu-card {
+  width: 300px;
+  height: 80px;
+  background-color: #ccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 24px;
+}
+
+.reservation-card {
+  width: 400px;
+  height: 60px;
+  background-color: rgb(255, 212, 147);
+  display: flex;
+  flex-direction: space-between;
+  align-items: center;
+  margin-bottom: 8px;
+}
+
+</style>
