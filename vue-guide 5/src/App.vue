34242a14a16b09e35d1e32c73134c052045edce7
devmatsuko <devmatsuko@gmail.com>
2022-05-06 19:52:02 +0900

    Use provide and inject.

---
+++ vue-guide 5/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import TodoList from './components/TodoList.vue';
+import { provide } from 'vue';
+import { todos, todoKey } from './useTodo'
+
+// 子コンポーネントが'todos'をキーにtodosの変数を受け取れるようになる
+// provide('todos', todos)
+provide(todoKey, todos)
+</script>
+
+<template>
+  <TodoList></TodoList>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue-guide 5/src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+import { todoKey } from '../useTodo'
+
+const state = inject(todoKey)
+if (!state) {
+  throw new Error('state is undefined')
+}
+// 親コンポーネントでprovideされた変数を受け取る
+const { todos, addTodo } = state
+// const _addTodo = inject('addTodo')
+
+</script>
+
+<template>
+  <div>
+    <ul :style="{ listStyle: 'none' }">
+      <li
+        :style="{ backgroundColor: 'aliceblue', marginBottom: '10px' }"
+        v-for="todo in todos"
+      >{{ todo.title }}</li>
+    </ul>
+    <button @click="addTodo('add!')">Add</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
